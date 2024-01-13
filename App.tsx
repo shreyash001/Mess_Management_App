3d0587f73298e61ccd4210ef2e79e89436b4eb47
Shreyash Singh <65494781+shreyash001@users.noreply.github.com>
2024-01-13 15:45:26 +0530

    Edit

--- App.tsx
+++
@@ -1,9 +0,0 @@
-// import {View, Text} from 'react-native';
-import React from 'react';
-import AppNavigation from './src/AppNavigation';
-
-const App = () => {
-  return <AppNavigation />;
-};
-
-export default App;
